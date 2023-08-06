f13790186a14cfe81143c15a7200505233f99fcf
iSpeedMan <tendetkinalesej@gmail.com>
2023-08-06 18:21:11 +0300

    add button

--- src/apps/slaider/admin.py
+++ src/apps/slaider/admin.py
@@ -5,7 +5,7 @@ from .models import Mems
 @admin.register(Mems)
 class MemsAdmin(admin.ModelAdmin):
     def get_image(self, Mems):
-        return mark_safe(f'<img src="{Mems.img.url}" alt="{Mems.name}" class="admin-icon"/>')
+        return mark_safe(f'<img width="50px" src="{Mems.img.url}" alt="{Mems.name}" class="admin-icon"/>')
 
     get_image.short_description = u'Привью'
     list_display = ('name', 'get_image')
